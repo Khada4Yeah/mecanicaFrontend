e656cab882dd2639b3563c150dfba7d71aa047a2
KhadaYeah <pespinoza9494@utm.edu.ec>
2024-06-27 14:32:15 -0500

    Arrelgo antes de independizar ramas

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,4 @@
 export const environment = {
     production: false,
-    //API_URL: 'http://localhost:8000/api/',
-    API_URL: 'https://app-mecanica-espinoza-backend-34fc43ce9a1f.herokuapp.com/api/'
-    //API_URL: 'https://1jz2hmlr-8000.use.devtunnels.ms/'
+    API_URL: 'http://localhost:8000/api/',
 };
